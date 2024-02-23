80e73cf934b07528abd0fa2932b7e1bfcfc990a8
davidsumsion <davidsumsion20@gmail.com>
2024-02-22 21:50:42 -0700

    created exception classes BadRequest and Forbidden

---
+++ server/src/main/java/services/BadRequestException.java
@@ -0,0 +1,5 @@
+package services;
+
+public class BadRequestException extends Exception{
+    public BadRequestException(String message) { super(message); }
+}
---
+++ server/src/main/java/services/ForbiddenException.java
@@ -0,0 +1,5 @@
+package services;
+
+public class ForbiddenException extends Exception{
+    public ForbiddenException(String message) { super(message); }
+}
