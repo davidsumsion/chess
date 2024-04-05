809b92c0162eecf58ba53f1f5847b3c046fe9e6f
James Finlinson <the30clues@gmail.com>
2024-04-05 14:20:45 -0600

    Trigger code from client/Main

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,8 +1,15 @@
 import chess.*;
+import ui.Client;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Client: " + piece);
+
+        try {
+            Client.main(new String[]{});
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
     }
 }
